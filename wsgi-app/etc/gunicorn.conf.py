6a79a631694586ec0b4ce88d4f0845a67c7fdbd5
Anton Vasyuk <75976878+antonvasyuk@users.noreply.github.com>
2021-07-15 23:48:17 +0300

    Delete wsgi-app directory

--- wsgi-app/etc/gunicorn.conf.py
+++
@@ -1,4 +0,0 @@
-app = '/home/box/web/hello:app'
-bind = '0.0.0.0:8080'
-pidfile = '/var/run/gunicorn.pid'
-workers = 5
--- wsgi-app/hello.py
+++
@@ -1,7 +0,0 @@
-def app(env, start_response):
-    body = env['QUERY_STRING'].split('&')
-    for i in range(len(body)):
-        body[i] += '\n'
-        body[i] = bytes(body[i], 'ascii')
-    start_response('200 OK', [('Content-type', 'text/plain')])
-    return body
